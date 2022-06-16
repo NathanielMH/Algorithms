f53930f5810ee174fe45475640e834e46b40e5e7
Nathaniel Mitrani <104577523+NathanielMH@users.noreply.github.com>
2022-06-16 17:51:56 +0200

    Update ff.cc

--- ff.cc
+++ ff.cc
@@ -7,73 +7,55 @@
 using Node = int;
 using namespace std;
 
-using Flow = vector<unordered_map<Node,Node>>; //value is flow
+using Flow = vector<unordered_map<Node,int>>; //value is flow
 using adjl = list<Node>;
 using Graph = vector<adjl>;
 
-void read_graph(int n,int m, Graph &G, Flow&F){
-    G = Graph(n);
-    F= Flow(n);
+void read_graph(int m, Graph &G, Flow&F){
     for (int i=0;i<m;i++){
         Node src,dst;
-        int fl;
-        cin>>src>>dst>>fl;
+        cin>>src>>dst>>F[src][dst];
         G[src].push_back(dst);
-        F[src][dst] = fl;
-        F[dst].insert({src,0}); //only happens if none was inserted before.
+        G[dst].push_back(src);
     }
 }
 
-bool bfs(const Graph &G, Node src, vector<Node> &pre, Flow&f){
-    vector<bool> queued(G.size(), false);
+bool bfs(const Graph &G, Node src, vector<Node> &pre, Flow&f, int &mf){
     queue<Node> s;
+    for (Node& v: pre) v = -1;
     s.push(src);
-    queued[src] = true;
-    pre[src] = -1;
-    while (not s.empty())
-    {
+    while (not s.empty()){
         int visiting = s.front();
         s.pop();
         for (auto it = G[visiting].begin(); it != G[visiting].end(); it++)
         {
-            if (not queued[*it] and  f[visiting][*it]!= 0)
+            if (f[visiting][*it]!= 0 and pre[*it]==-1)
             {
                 s.push(*it);
-                queued[*it] = true;
                 pre[*it] = visiting;
+                mf = min(mf,f[visiting][*it]);
+                if (*it == pre.size()-1) return true;
             }
         }
     }
-    return queued[G.size()-1];
+    return false;
 }
 
-int min_edge(const vector<Node>&pre, Flow&f){
-    Node src = 0, dst = pre.size()-1;
-    int mf=100000;
-    for (Node u = dst; u!=src; u=pre[u]){
-        Node v = pre[u];
-        mf = min(mf,f[v][u]);
-    }
-    return mf;
-}
-
-void update_edges(Flow &f, vector<Node>&pre, int mf, Graph &G){
+void update_edges(Flow&f,vector<Node>&pre, int mf){
     Node src = 0, dst = pre.size()-1;
     for (Node u = dst; u!=src; u=pre[u]){
         Node v = pre[u];
         f[v][u] -= mf;
         f[u][v] += mf;
-        if (f[u][v] == mf) G[u].push_back(v);
     }
 }
 
 int ford_fulkerson(Graph &G, Flow &f){
-    int maxflow = 0,n=G.size();
-    vector<Node>pre(G.size());
-    while (bfs(G,0,pre,f)){
-        int mf = min_edge(pre,f);
+    int maxflow = 0,mf=100000;
+    vector<Node>pre(G.size(),-1);
+    while (bfs(G,0,pre,f,mf)){
         maxflow+=mf;
-        update_edges(f,pre,mf,G);
+        update_edges(f,pre,mf);
     }
     return maxflow;
 }
@@ -81,9 +63,9 @@ int ford_fulkerson(Graph &G, Flow &f){
 int main (){
 int n,m;
 while (cin>>n>>m){
-    Graph G;
-    Flow f;
-    read_graph(n,m,G,f);
+    Graph G(n);
+    Flow f(n);
+    read_graph(m,G,f);
     cout<<ford_fulkerson(G,f)<<endl;
 }
 }
